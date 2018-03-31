4d5dbf38263ef7ad6b45dee313a717e1db72c67b
nghifarii <30855036+nghifarii@users.noreply.github.com>
2018-03-31 17:23:50 +0800

    Update PeopleCount.py

--- PeopleCount.py
+++ PeopleCount.py
@@ -9,12 +9,27 @@ class Orang:
    
   def __del__(self):
     #kurangi total orang jika objek di hapus
-    orang.total -= 1
+    orang.total_orang -= 1
   
   def katakanHalo(self):
     print ("Halo, nama saya %s, apa kabar? " % self.nama)
   
   def total_populasi(self):
     print ("total orang %s " cls.total)
+  
+  #method class
+  total_populasi = classmethod(total_populasi)
     
-   
+org = Orang ("Bubu")
+org.katakanHalo()
+Orang.total_pupolasi
+
+org2 = Orang("Dede")
+org2.katakanHelo()
+Orang.total_populasi
+
+print ("objek dihapus, sekarang tinggal ")
+del org
+del org2
+
+Orang.total_populasi()
