3cc5afb10b2e22f7dc00fe7db93fa7ab5dd58fd5
nghifari <30855036+nghifarii@users.noreply.github.com>
2018-04-04 22:57:53 +0800

    Create contohEnum.py

---
+++ contohEnum.py
@@ -0,0 +1,22 @@
+# Python
+from enum import Enumenum 
+class AnimalEnum(Enum):
+    Kuda = 1
+    Sapi = 2
+    Ayam = 3
+    Kelinci = 4
+print (AnimalEnum.Aya,)
+print (repr(AnimalEnum.Ayam))
+print (AnimalEnum.Ayam.name)
+print (AnimalEnum.ayam.value)
+
+for animal in AnimalEnum:
+    print ("Name: {} value: {}".format(animal, animal.value))
+    
+
+""""
+Disini kita membuan sebuah Enumeration class dengan nama AnimalEnum.
+di dalam class, kita membuat class attributes dengan nama-nama member enumeration, yang konstan.
+ketika kamu mencoba untuk print out sebuah member, kamu hanya akan mendapatkan kembali string. 
+tapi jika kmau print out the repr dari enum member, kamu akan mendapatkan enum member dan nilainya.
+"""
