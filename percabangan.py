79180e24938ce343bf4ec8d59299d316a8f1ff0d
nghifari <30855036+nghifarii@users.noreply.github.com>
2018-05-23 22:31:17 +0800

    Update percabangan.py

--- percabangan.py
+++ percabangan.py
@@ -8,3 +8,29 @@ else:
   print ("Kamu harus remedial")
   
   
+# Contoh 2
+
+# progran mengecek bonus dan diskon
+
+total_belanja = input ("total belanja: Rp ")
+
+# jumlah yang harus dibayar adalah berapa total belanjaannya
+# tapi kalau dapat diskon akan berkurang
+
+bayar = total_belanja
+
+# jika dia belanja di atas 100rb maka berikan bonus dan diskon
+if total_belanja > 100000:
+  print ("kamu mendapatkan bonus minuman dingin")
+  print ("dan diskon 5% ")
+  
+  # hitung diskonnya
+  diskon = total_belanja * 5/100 # 5%
+  bayar = total_belanja - diskon
+ 
+
+#cetak struk
+print ("Total yang harus dibayar: Rp %s" % bayar)
+print ("Terima kasih sudah berbelanja ")
+print ("Datang lagi yaa")
+
